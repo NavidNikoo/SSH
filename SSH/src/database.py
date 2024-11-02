edb150195b3e4df512d6efce4638fcc1d3e119f1
smminassian <sebastienminassian203@gmail.com>
2024-11-02 16:18:20 -0700

    created database server, signup, and login

---
+++ SSH/src/database.py
@@ -0,0 +1,72 @@
+import socket
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad
+from Cryptodome.Util.Padding import unpad
+
+
+
+#I Implement Command line arguments here. If there are n arguments present in command line
+#execute the code below
+
+
+# The key (must be 16 bytes)
+key = b'Sixteen byte key'
+port = 1235
+
+# Create a socket
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+
+# Associate the socket with the port
+serverSock.bind(('', port)) 
+
+
+# Start listening for incoming connections (we can have
+# at most 100 connections waiting to be accepted before
+# the server starts rejecting new connections)
+serverSock.listen(4)
+
+
+# Keep accepting connections forever
+while True:
+
+
+	print("Waiting for clients to connect...")
+	
+		# Accept a waiting connection
+	cliSock, cliInfo = serverSock.accept()
+	
+	print("Client connected from: " + str(cliInfo))
+	
+		# Receive the data the client has to send.
+		# This will receive at most 1024 bytes
+	data = cliSock.recv(1024)
+	
+	#Read IV
+	iv = data[:AES.block_size]
+
+	decCipher = AES.new(key, AES.MODE_ECB, iv)
+
+	username_length = data[:AES.block_size]
+	username_start = AES.block_size + 1
+	username_end = username_start + username_length
+	username_cipher = data[username_start:username_end]
+
+	password_length = data[:AES.block_size]
+	password_start = username_end + 1
+	password_cipher = data[password_start:password_start + password_length]
+
+	
+	encrypted_username = data[AES.block_size:AES.block_size*2]
+	encrypted_password = data[AES.block_size*2]
+
+
+	decrypted_username = unpad(decCipher.decrypt(encrypted_username), AES.block_size).decode()
+	decrypted_password = unpad(decCipher.decrypt(encrypted_password), AES.block_size).decode()	
+	
+
+	print("Username: " + str(decrypted_username))
+	print("Password: " + str(decrypted_password))
+
+	# Hang up the client's connection
+	cliSock.close()
---
+++ SSH/src/signUp.py
@@ -0,0 +1,57 @@
+import socket
+from Cryptodome import Random
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import pad
+from Cryptodome.Util.Padding import unpad
+
+#I Implement Command line arguments here. If there are n arguments present in command line
+#execute the code below
+
+	# The key (must be 16 bytes)
+key = b'Sixteen byte key'
+port = 1235
+Server_IP = "127.0.0.1"
+	
+# Set up the AES encryption class
+iv = Random.new().read(AES.block_size)
+encCipher = AES.new(key, AES.MODE_CBC, iv)
+
+Username = input('Username: ')
+password = input('Password: ')
+
+# The client's socket
+cliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# Attempt to connect to the server
+cliSock.connect((Server_IP, port))
+#server needs to be started first and then client
+
+# Send the message to the server
+# NOTE: the user input is of type string
+# Sending data over the socket requires.
+# First converting the string into bytes.
+# encode() function achieves this.
+Username = pad(Username.encode(), AES.block_size) #pad the message as a byte and it will be blocks of bytes. The reason why it is blocks of bytes is because I am using AES which encrypts and decrypts using block cipher
+password = pad(password.encode(), AES.block_size)
+
+#encrypt the padded message
+userNameCipher = encCipher.encrypt(Username)
+passwordCipher = encCipher.encrypt(password)
+
+#Find length of username 
+username_length = len(userNameCipher)
+password_length = len(passwordCipher)
+
+
+
+	# Send the message to the server
+cliSock.send(iv + bytes[username_length] + userNameCipher + bytes[password_length] + passwordCipher)
+
+
+print(userNameCipher + iv)
+print(passwordCipher + iv)
+
+
+
+
